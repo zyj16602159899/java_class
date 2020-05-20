5a1eb38f00499dfc47c486a7d4ec4089d81754f4
zyj16602159899 <z16602159899@163.com>
2020-05-20 12:54:20 +0800

    520

--- CK/ck_0518.py
+++ CK/ck_0518.py
@@ -1,13 +1,33 @@
 # 自定义属性访问
 
+# __getattr__方法和__getattribute__方法都存在的时候，先执行__getattribute__方法
+
 class Test(object):
 
-    def __getattr__(self, item):        # 访问属性的时候，如果属性不存在，该方法会触发。
+    def __init__(self):
+        self.age = 18
+
+    def __getattr__(self, item):        # 访问属性的时候，如果属性不存在(出现AttrError)，会触发该方法。
         print('这是getattr')
+        # object.__getattribute__(self, item)     # 这是父类的方法
         return 200
+    
+    def __getattribute__(self, item):   # 访问属性的时候，第一时间触发该方法去查找属性
+        print('这是__getattribute__方法')
+        # return 500
+        return super().__getattribute__(item)
+
+    def __setattr__(self, key, value):  # 这个方法是在给对象设置属性的时候触发(可以在设置属性的时候干扰操作)
+        if key == 'age':
+            super().__setattr__(key, 18)
+        else:
+            print('设置属性时触发该方法！')
+            super().__setattr__(key, value)
+
 
 
 t = Test()
 t.name = 100
+t.age = 888
 print(t.name)
 print(t.age)        # 访问属性的时候，如果属性不存在，会触发__getattr__方法。
